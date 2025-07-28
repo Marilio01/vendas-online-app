5d207dab062cc6fa191a7836635591c9aa5b2cd0
Marilio -Marilio01 <Mariliocavalcanti16@gmail.com>
2025-07-28 09:29:11 -0300

    feat:insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: #ca1212;
+`;
 
 const App = () => {
-    return (
-        <View>
-            <Text>TESTANDO</Text>
-        </View>
-    )
+  return (
+    <SafeAreaView>
+      <Text>Testando</Text>
+      <Text style={styles.container}>Testando</Text>
+      <TextNew>NOVO TESTE</TextNew>
+    </SafeAreaView>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+});
+
 export default App;
-
-
-
