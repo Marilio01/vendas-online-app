2ccf716c0c0de3e6705faa231b4587548f0267f4
Marilio -Marilio01 <Mariliocavalcanti16@gmail.com>
2025-08-05 09:06:52 -0300

    feat: create splash screen

--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -3,10 +3,21 @@ import { useState } from 'react';
 import { useUserReducer } from '../../store/reducers/userReducer/useUserReducer';
 import { useGlobalReducer } from '../../store/reducers/globalReducer/useGlobalReducer';
 import { setAuthorizationToken } from '../functions/connection/auth';
-import { connectionAPIPost } from '../functions/connection/connectionAPI';
+import ConnectionAPI, {
+    connectionAPIPost,
+    MethodType,
+} from '../functions/connection/connectionAPI';
 import { RequestLogin } from '../types/requestLogin';
 import { ReturnLogin } from '../types/returnLogin';
 import { MenuUrl } from '../enums/MenuUrl.enum';
+
+interface requestProps<T> {
+    url: string;
+    method: MethodType;
+    saveGlobal?: (object: T) => void;
+    body?: unknown;
+    message?: string;
+}
 
 export const useRequest = () => {
     const { reset } = useNavigation<NavigationProp<ParamListBase>>();
@@ -14,6 +25,42 @@ export const useRequest = () => {
     const { setModal } = useGlobalReducer();
     const [loading, setLoading] = useState<boolean>(false);
     const [errorMessage, setErrorMessage] = useState<string>('');
+
+    const request = async <T>({
+        url,
+        method,
+        saveGlobal,
+        body,
+        message,
+    }: requestProps<T>): Promise<T | undefined> => {
+        setLoading(true);
+        const returnObject: T | undefined = await ConnectionAPI.connect<T>(url, method, body)
+            .then((result) => {
+                if (saveGlobal) {
+                    saveGlobal(result);
+                }
+                if (message) {
+                    setModal({
+                        visible: true,
+                        title: 'Sucesso!',
+                        text: message,
+                    });
+                }
+                return result;
+            })
+            .catch((error: Error) => {
+                setModal({
+                    visible: true,
+                    title: 'Erro',
+                    text: error.message,
+                });
+                return undefined;
+            });
+
+        setLoading(false);
+        return returnObject;
+    };
+
 
     const authRequest = async (body: RequestLogin) => {
         setLoading(true);
@@ -39,6 +86,7 @@ export const useRequest = () => {
     return {
         loading,
         errorMessage,
+        request,
         authRequest,
         setErrorMessage,
     };
