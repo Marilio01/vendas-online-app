49b74bac7dd4f1bc8653a3a82b7e84e839ad9b0e
Marilio -Marilio01 <Mariliocavalcanti16@gmail.com>
2025-08-06 13:53:56 -0300

    feat: insert input in search screen

--- src/modules/searchProduct/screen/SearchProduct.tsx
+++ src/modules/searchProduct/screen/SearchProduct.tsx
@@ -1,14 +1,18 @@
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { NativeSyntheticEvent, ScrollView, TextInputChangeEventData } from 'react-native';
 
 import { MethodEnum } from '../../../enums/methods.enum';
+import Input from '../../../shared/components/input/Input';
+
 import Text from '../../../shared/components/text/Text';
 import { URL_PRODUCT_PAGE } from '../../../shared/constants/urls';
 import { useRequest } from '../../../shared/hooks/useRequest';
 import { PaginationType } from '../../../shared/types/paginationType';
 import { ProductType } from '../../../shared/types/productType';
 import { useProductReducer } from '../../../store/reducers/productReducer/useProductReducer';
+import ProductThumbnail from '../../../shared/components/productThumbnail.tsx/ProductThumbnail';
 
 export type SearchProductNavigationProp = NativeStackNavigationProp<
     Record<string, SearchProductParams>
@@ -21,20 +25,35 @@ export interface SearchProductParams {
 const SearchProduct = () => {
     const { searchProducts, setSearchProducts } = useProductReducer();
     const { params } = useRoute<RouteProp<Record<string, SearchProductParams>>>();
-    const { search } = params;
     const { request } = useRequest();
+    const [value, setValue] = useState(params?.search || '');
+
+    useEffect(() => {
+        setValue(params?.search || '');
+    }, [params]);
 
     useEffect(() => {
         request<PaginationType<ProductType[]>>({
-            url: `${URL_PRODUCT_PAGE}?search=${search}`,
+            url: `${URL_PRODUCT_PAGE}?search=${value}`,
             method: MethodEnum.GET,
             saveGlobal: setSearchProducts,
         });
-    }, [search]);
+    }, [value]);
+
+    const handleOnChangeInput = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        setValue(event.nativeEvent.text);
+    };
 
     return (
         <>
-            {searchProducts && <Text>Tem produto</Text>}
+            <Input onChange={handleOnChangeInput} value={value} iconRight="search" />
+            {searchProducts && searchProducts.data && (
+                <ScrollView>
+                    {searchProducts.data.map((product) => (
+                        <ProductThumbnail product={product} />
+                    ))}
+                </ScrollView>
+            )}
             <Text>Qualquer coisa</Text>
         </>
     );
