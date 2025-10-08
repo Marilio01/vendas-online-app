382049ee11d8bc7bd885a5e1bf2b0be0a6990171
joao769 <joaobarbosavictor132@gmail.com>
2025-10-08 00:30:17 -0300

    style: update design in profile

--- src/modules/profile/screens/Profile.tsx
+++ src/modules/profile/screens/Profile.tsx
@@ -1,18 +1,20 @@
 import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Button from '../../../shared/components/button/Button';
-import Text from '../../../shared/components/text/Text';
 import { logout } from '../../../shared/functions/connection/auth';
 
 const Profile = () => {
-    const navigate = useNavigation<NavigationProp<ParamListBase>>();
+  const navigate = useNavigation<NavigationProp<ParamListBase>>();
 
-    return (
-        <>
-            <Text>Profile</Text>
-            <Button title="sair" onPress={() => logout(navigate)} />
-        </>
-    );
+  return (
+    <SafeAreaView>
+      <View>
+        <Button title="Sair" onPress={() => logout(navigate)} />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Profile;
