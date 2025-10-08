106bd6c5d3893a4e4223ef5007b6e12207e2dc6a
joao769 <joaobarbosavictor132@gmail.com>
2025-10-08 00:20:39 -0300

    feat: set up ThemeProvider for global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 
 import { Provider } from 'react-redux';
-
 import Navigation from './Navigation';
-
 import store from './store';
 import GlobalModal from './shared/components/modal/globalModal/GlobalModal';
-
-
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './shared/themes/theme';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <GlobalModal />
-      <Navigation />
-    </Provider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <GlobalModal />
+          <Navigation />
+          </Provider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 };
 
